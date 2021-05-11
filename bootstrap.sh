47e3454cc36ae8f9e8140026af0590df7de93d72
wpbindt <wesselbindt@gmail.com>
2021-05-11 23:50:25 +0200

    I MAKE STUFF

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,14 @@
 DOTFILES=$(pwd)/dotfiles
 
+for SUBDIR in $(find $DOTFILES -type d -printf '%P\n')
+do
+    mkdir -p $HOME/$SUBDIR
+done
+
 for FILENAME in $(find $DOTFILES -type f -printf '%P\n')
 do
     DOTFILE=$DOTFILES$FILENAME
     ORIGINAL_FILE=$HOME/$FILENAME
-    echo "$FILE"
     if [ ! $ORIGINAL_FILE -ef $DOTFILE ] ; then
         [ -e $ORIGINAL_FILE ] && mv $ORIGINAL_FILE $ORIGINAL_FILE.local
         touch $ORIGINAL_FILE.local

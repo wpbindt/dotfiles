a8cf16d4b6bcd19a447ceda1237915a8c6f4c153
wpbindt <wesselbindt@gmail.com>
2021-05-13 00:18:10 +0200

    Snake case variable names

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,22 @@
-DOTFILES=$(pwd)/dotfiles
+dotfiles=$(pwd)/dotfiles
 
 function list_dotfile_contents {
-    find $DOTFILES -type $1 -printf '%P\n'
+    find $dotfiles -type $1 -printf '%P\n'
 }
 
-for SUBDIR in $(list_dotfile_contents d)
+for subdir in $(list_dotfile_contents d)
 do
-    mkdir -p $HOME/$SUBDIR
+    mkdir -p $HOME/$subdir
 done
 
-for FILENAME in $(list_dotfile_contents f)
+for filename in $(list_dotfile_contents f)
 do
-    DOTFILE=$DOTFILES/$FILENAME
-    ORIGINAL_FILE=$HOME/$FILENAME
-    if [ ! $ORIGINAL_FILE -ef $DOTFILE ] ; then
-        [ -e $ORIGINAL_FILE ] && mv $ORIGINAL_FILE $ORIGINAL_FILE.local
-        touch $ORIGINAL_FILE.local
-        ln -s $DOTFILE $ORIGINAL_FILE
+    dotfile=$dotfiles/$filename
+    original_file=$HOME/$filename
+    if [ ! $original_file -ef $dotfile ] ; then
+        [ -e $original_file ] && mv $original_file $original_file.local
+        touch $original_file.local
+        ln -s $dotfile $original_file
     fi
 done
 

5d3ec8614ac11977896a878012116c60151e0a75
wpbindt <wesselbindt@gmail.com>
2021-07-28 23:29:08 +0200

    Add xmobar

--- dotfiles/.xmonad/xmonad.hs
+++ dotfiles/.xmonad/xmonad.hs
@@ -1,5 +1,7 @@
 import XMonad
+import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.SetWMName
+import XMonad.Util.Run
 import XMonad.Util.SpawnOnce
 import Data.Monoid
 import System.Exit
@@ -145,7 +147,7 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 -- The available layouts.  Note that each layout is separated by |||,
 -- which denotes layout choice.
 --
-myLayout = tiled ||| Mirror tiled ||| Full
+myLayout = avoidStruts (tiled ||| Mirror tiled ||| Full)
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
@@ -217,7 +219,9 @@ myStartupHook = do
 
 -- Run xmonad with the settings you specify. No need to modify this.
 --
-main = xmonad defaults
+main = do
+        xmproc <- spawnPipe "xmobar -x 0"
+        xmonad $ docks defaults
 
 -- A structure containing your configuration settings, overriding
 -- fields in the default config. Any you don't override, will
